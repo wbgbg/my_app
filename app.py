6fea99adb478c4c56044ca45f7b3ae42dd812f82
wbgbg <wbgbg123@gmail.com>
2015-04-18 22:28:15 +0800

    bug

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,BadSignature,SignatureExpired)
+#from uwsgidecorators import *
 
 app = Flask(__name__)
 app.debug = True
@@ -138,29 +139,31 @@ def favorite():
 		respon[product.hkd]=product.price
 	return(jsonify(respon),200,())
 		
-@timer(refresh_time)
-#@app.route('/api/fetch',methods=['POST','GET'])
-def fetch_product():
-	for now_product in JDProduct.query.all():
-		ukd="J_"+"%d" % now_product.hkd
-		apipath="https://api.jd.com/routerjson?v=2.0&"
-		method="method=jingdong.ware.price.get&"
-		app_key="app_key=84F0963912EA9D56CD29E8EB3E774A2B&"
-		sku_id="360buy_param_json={%22sku_id%22:%22"+ukd+"%22}&"
-		now=time.localtime()
-		timestamp="timestamp="+time.strftime("%Y-%m-%d %X",now)
-		url=apipath+method+app_key+sku_id+timestamp
-		f = urllib.urlopen(url)
-		st = f.read()
-		js = json.loads(st)
-		price = js[u"jingdong_ware_price_get_responce"][u"price_changes"][0][u"price"]
-		fil = open("/home/nowlog",'a')
-		fil.write(price)
-		fil.close
+#@timer(refresh_time)
+@app.route('/api/fetch/<int:hkd>',methods=['GET'])
+def fetch_product(hkd):
+	#for now_product in JDProduct.query.all():
+	ukd="J_"+"%d" %hkd
+	apipath="https://api.jd.com/routerjson?v=2.0&"
+	method="method=jingdong.ware.price.get&"
+	app_key="app_key=84F0963912EA9D56CD29E8EB3E774A2B&"
+	sku_id="360buy_param_json={%22sku_id%22:%22"+ukd+"%22}&"
+	now=time.localtime()
+	timestamp="timestamp="+time.strftime("%Y-%m-%d %X",now)
+	url=apipath+method+app_key+sku_id+timestamp
+	f = urllib.urlopen(url)
+	st = f.read()
+	js = json.loads(st)
+	price = js[u"jingdong_ware_price_get_responce"][u"price_changes"][0][u"price"]
+	fil = open("/home/nowlog",'a')
+	fil.write(price)
+	fil.close
+	now_product = JDProduct.query.filter_by(hkd=hkd).first()
+	if now_product is not None:
 		if now_product.price != price:
 			now_product.price = price
-			tell_client(nowproduct.ukd)
-			
+			tell_client(nowproduct.hkd)
+	return ukd,price		
 def tell_client(ukd):
 	pass
 
