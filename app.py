0264378cdf0d547e32696ad9cef55f59e41e7cb5
wbgbg <wbgbg123@gmail.com>
2015-04-21 01:28:18 +0800

    add commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
-#elncoding = utf-8
+#coding=utf-8
+#encoding=utf-8
 from gevent import monkey; monkey.patch_all()
 import gevent
 import os,sys
@@ -20,12 +21,12 @@ from email.header import Header
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'CZH IS DIAO BAO LE'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/db.sqlite'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-refresh_time = 60
+app.config['SECRET_KEY'] = 'CZH IS DIAO BAO LE'   #密钥
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/db.sqlite'   #数据库地址
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  #request后自动提交 
+#refresh_time = 60   
 apipath="https://api.jd.com/routerjson?v=2.0&"
-app_key="app_key=84F0963912EA9D56CD29E8EB3E774A2B&"
+app_key="app_key=84F0963912EA9D56CD29E8EB3E774A2B&"  #拼接url请求京东API用
 
 #class Config(object):
 #
@@ -58,9 +59,10 @@ auth=HTTPBasicAuth()
 
 user_jdproducts= db.Table('user_jdproducts',
 	db.Column('User_id',db.Integer,db.ForeignKey('users.id')),
-	db.Column('JDProduct_id',db.Integer,db.ForeignKey('jdproducts.id')))
+	db.Column('JDProduct_id',db.Integer,db.ForeignKey('jdproducts.id')))  #账号与商品的关系表,用于完成两表间多对多关系
 
 class User(db.Model):
+	"""账号数据表类,包含主键,账号,哈希过的密码,以及连接商品用的外键"""
 	__tablename__='users'
 	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String(32),index=True)
@@ -72,15 +74,19 @@ class User(db.Model):
 #		self.password=password
 		
 	def hash_password(self,password):
+		"""哈希加密密码"""
 		self.password_hash = pwd_context.encrypt(password)
 	def verify_password(self,password):
+		"""验证密码"""
 		return pwd_context.verify(password,self.password_hash)
 	def generate_auth_token(self,expiration=600):
+		"""生成登录后的验证token"""
 		s=Serializer(app.config['SECRET_KEY'], expires_in=expiration) 
 		return s.dumps({'id':self.id})
 	
 	@staticmethod
 	def verify_auth_token(token):
+		"""验证token是否有效"""
 		s = Serializer(app.config['SECRET_KEY'])
 		try:
 			data = s.loads(token)
@@ -93,6 +99,7 @@ class User(db.Model):
 
 @auth.verify_password
 def verify_password(username_or_token,password):
+	"""验证密码,调用类中的比较函数进行比较"""
 	user = User.verify_auth_token(username_or_token)
 	if not user:
 		user = User.query.filter_by(username=username_or_token).first()
@@ -102,6 +109,7 @@ def verify_password(username_or_token,password):
 	return True
 
 class JDProduct(db.Model):
+	"""商品信息数据表,包含主键,商品编号,价格以及一个备用的列"""
 	__tablename__='jdproducts'
 	id = db.Column(db.Integer,primary_key=True)
 	hkd = db.Column(db.String(10))
@@ -111,6 +119,7 @@ class JDProduct(db.Model):
 @app.route('/api/favorite/add',methods=['POST'])
 @auth.login_required
 def add_favorite():
+	"""添加收藏API"""
 	user=g.user
 	print user.jdproducts
 	producthkd = request.json.get('hkd')
@@ -131,7 +140,7 @@ def add_favorite():
 @app.route('/api/favorite/del',methods=['POST'])
 @auth.login_required
 def del_favorite():	
-	
+	"""删除收藏API"""
 	producthkd = request.json.get('hkd')
 	if JDProduct.query.filter_by(hkd=producthkd).first() is not None:
 		product = JDProduct.query.filter_by(hkd=producthkd).first()
@@ -140,9 +149,11 @@ def del_favorite():
 #		db.session.add(user)
 #		db.session.commit()
 	return "ok"
+
 @app.route('/api/favorite',methods=['GET'])
 @auth.login_required
 def favorite():
+	"""查询用户收藏商品API"""
 	user = g.user
 	respon ={}
 	for product in user.jdproducts:
@@ -151,13 +162,16 @@ def favorite():
 
 @app.route('/api/jdproducts/<hkd>',methods=['GET'])
 def jdpro(hkd):
+	"""查询商品价格API"""
 	respon ={}
 	for user in user.jdproducts:
 		respon[product.hkd]=product.price
 	return(jsonify(respon),200,())
 
-@app.route('/api/jdproducts/price/<hkd>',methods=['GET'])
+@app.route('/api/jdproducts/name/<hkd>',methods=['GET'])
+@auth.login_required
 def product_price(hkd):
+	"""向京东请求商品名称api"""
 	respon = {}
 	method = "method=jingdong.ware.baseproduct.get&"	
 	sku_id = "360buy_param_json={%22ids%22:%22"+hkd+"%22,%22base%22:%22name%22}&"
@@ -180,6 +194,7 @@ def product_price(hkd):
 #timer(refresh_time)
 @app.route('/api/fetch/all')
 def fetch_all():
+	"""对数据库中所有商品向京东请求价格,并修改数据库,发送提醒,协程实现异步io"""
 	method="method=jingdong.ware.price.get&"
 	i=1
 	print "start working"
@@ -195,13 +210,15 @@ def fetch_all():
 	return "ok"
 
 def three_minute_call(signum):
+	"""定时任务 每10分钟执行一次fetch_all"""
 	fetch_all()
 
-uwsgi.register_signal(99,"",three_minute_call)
-uwsgi.add_timer(99,600)
+uwsgi.register_signal(99,"",three_minute_call)#信号注册
+uwsgi.add_timer(99,600)#定时器
 
 @app.route('/api/sendmail')
 def sendmail(hkd=0,price1=0,price2=0):
+	"""demo 发送邮件提醒价格变动"""
         sender = "wbgbg123@126.com"
         receiver = "576214465@qq.com"
         subject = "the price of %s has changed from %s to %s" % (hkd,price1,price2)
@@ -224,6 +241,7 @@ def sendmail(hkd=0,price1=0,price2=0):
 
 @app.route('/api/fetch/<hkd>')
 def fetch_product(hkd=0):
+	"""向京东查询单个商品价格"""
 	#for now_product in JDProduct.query.all():
 	ukd="J_%s"%hkd
 	method="method=jingdong.ware.price.get&"
@@ -241,7 +259,7 @@ def fetch_product(hkd=0):
 #	fil.close
 	now_product = JDProduct.query.filter_by(hkd=hkd).first()
 	if now_product is not None:
-		if (now_product.price != price and (now_product.price!="-1.00" or price != "-1")):
+		if (now_product.price != price and not(now_priduct.price in ["-1.00","-1"] and price in ["-1.00","-1"])):
 			print "%s price change from %s to %s" %(ukd,now_product.price,price)
 			now_product.price = price
 			sendmail(now_product.hkd,now_product.price,price)
@@ -255,7 +273,7 @@ def tell_client(ukd):
 
 @app.route('/api/users',methods=['POST'])	
 def new_user():
-	
+	"""新用户注册api"""
 	username = request.json.get('username')
 	password = request.json.get('password')
 	if username is None or password is None:
@@ -274,6 +292,7 @@ def new_user():
 
 @app.route('/api/users/<int:id>')	
 def get_user(id):
+	"""通过用户id查询用户名称api"""
 	user = User.query.get(id)
 	if not user:
 		abort(400)
@@ -282,6 +301,7 @@ def get_user(id):
 @app.route('/api/token')
 @auth.login_required
 def get_auth_token():
+	"""返回登录token API"""
 	token = g.user.generate_auth_token(600)
 	return jsonify({'token':token.decode('ascii'),'duration':600})
 
@@ -292,7 +312,7 @@ def get_resource():
 
 @app.route('/')
 def hello_world():
-	return u'好喜欢田艳斐啊'
+	return u'你好'
 @app.route('/czh')
 def czh():
 	return 'sb!'
